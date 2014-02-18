4006f35dd774c8df5b596e062fd87d90b3ae86f7
Joel Kemp <mrjoelkemp@gmail.com>
2014-02-18 14:24:29 -0500

    Ranking with html results page generation

--- YelpResult.js
+++ YelpResult.js
@@ -1,20 +1,42 @@
-var cheerio = require('cheerio');
+var cheerio = require('cheerio'),
+    request = require('request');
 
-module.exports = function (options) {
-  if (! options || ! options.html) {
-    this.html         = '';
-    this.rating       = 0;
-    this.numReviews   = 0;
-    this.name         = '';
+module.exports = function (name) {
+  this.name         = name || '';
+  this.html         = '';
+  this.rating       = 0;
+  this.numReviews   = 0;
+};
 
-    return;
-  }
+module.exports.prototype.ranking = function () {
+  return this.numReviews * this.rating;
+};
 
-  var $ = cheerio.load(options.html);
+module.exports.prototype.fetch = function (cb) {
+  this.getYelpResultHTML(this.name, function (html) {
+    var $ = cheerio.load(html),
+        $result;
 
-  this.html       = options.html;
-  this.rating     = parseFloat($('i.star-img').attr('title'));
-  this.numReviews = parseInt($('span.review-count').text(), 10);
-  this.name       = options.restaurantName || $('a.biz-name').text();
-  this.url        = 'http://www.yelp.com' + $('a.biz-name').attr('href');
+    $result = $('[data-key=1]');
+
+    $('.search-result').each(function (idx, el) {
+      console.log($(el).find('a.biz-name').text());
+    });
+
+    this.html       = $result.html();
+    this.rating     = parseFloat($result.find('i.star-img').attr('title'));
+    this.numReviews = parseInt($result.find('span.review-count').text(), 10);
+    this.url        = 'http://www.yelp.com' + $result.find('a.biz-name').attr('href');
+
+    cb && cb();
+
+  }.bind(this));
+};
+
+module.exports.prototype.getYelpResultHTML = function (restaurantName, cb) {
+  var url = 'http://www.yelp.com/search?find_desc=' + encodeURI(restaurantName) + '&find_loc=New+York';
+  console.log(restaurantName + ': ' + url);
+  request(url, function (err, resp, body) {
+    cb && cb(body);
+  });
 };
--- getRestaurants.js
+++ getRestaurants.js
@@ -1,26 +1,6 @@
-var request   = require('request'),
-    cheerio   = require('cheerio'),
-    YelpResult = require('./YelpResult');
+var Helpers   = require('./Helpers');
 
+// Helpers.saveRestaurantsPage();
+// Helpers.saveRestaurantNames();
 
-getYelpSearchResult('Dhaba', function (yelpObj) {
-  console.log(yelpObj)
-});
-
-function getYelpSearchResult (restaurantName, cb) {
-  if (! restaurantName) return;
-
-  var url = 'http://www.yelp.com/search?find_desc=' + encodeURI(restaurantName) + '&find_loc=New+York';
-
-  request(url, function (err, resp, body) {
-    // cb && cb(body);
-    var $ = cheerio.load(body),
-        yelpResult = new YelpResult({
-          name: restaurantName,
-          html: $('div.search-result').first().html()
-        });
-
-    cb && cb(yelpResult);
-  });
-}
-
+Helpers.generateResultsPage();
